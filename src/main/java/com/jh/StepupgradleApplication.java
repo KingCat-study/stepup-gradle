0e1c8199e69ecc239808a6020c0994ed320dc192
raiden <jihunrock@gmail.com>
2023-01-17 21:10:06 +0900

    최초 커밋

---
+++ src/main/java/com/jh/StepupgradleApplication.java
@@ -0,0 +1,11 @@
+package com.jh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepupgradleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StepupgradleApplication.class, args);
+    }
+}
