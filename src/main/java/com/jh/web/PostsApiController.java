1eb616a68c040ebb02d68f3aa132ee2bc0f81e57
raiden <jihunrock@gmail.com>
2023-01-18 21:26:54 +0900

    update findbyId 기능 추가

--- src/main/java/com/jh/web/PostsApiController.java
+++ src/main/java/com/jh/web/PostsApiController.java
@@ -1,9 +1,16 @@
 package com.jh.web;
 
 import com.jh.service.PostsService;
+import com.jh.web.dto.PostsResponseDto;
 import com.jh.web.dto.PostsSaveRequestDto;
+import com.jh.web.dto.PostsUpdateRequestDto;
+
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +25,17 @@ public class PostsApiController {
     @PostMapping("/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/v1/posts/{id}")
+    public Long update(
+        @PathVariable Long id, 
+        @RequestBody PostsUpdateRequestDto requestDto) {
+            return postsService.update(id, requestDto);
+        }
+
+    @GetMapping("/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
--- src/test/java/com/jh/web/PostsApiControllerTest.java
+++ src/test/java/com/jh/web/PostsApiControllerTest.java
@@ -1,8 +1,9 @@
 package com.jh.web;
 
-import com.jh.domain.posts.Posts;
-import com.jh.domain.posts.PostsRepository;
-import com.jh.web.dto.PostsSaveRequestDto;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +12,10 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.event.annotation.AfterTestMethod;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import com.jh.domain.posts.Posts;
+import com.jh.domain.posts.PostsRepository;
+import com.jh.web.dto.PostsSaveRequestDto;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
