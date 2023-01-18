f53195b1d1e95ea9d60ddd4eeabbec22420d0f61
jihunrock <jihunrock@gmail.com>
2023-01-18 13:06:25 +0900

    post 저장조회 테스트 기능 완성

--- src/test/java/com/jh/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jh/domain/posts/PostsRepositoryTest.java
@@ -4,33 +4,46 @@ import org.aspectj.lang.annotation.After;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.AfterTestMethod;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class PostsRepositoryTest {
 
     @Autowired
     PostsRepository postsRepository;
 
-    @AfterAll
-    public void cleanUp() {
+    @AfterTestMethod
+    public void cleanup() {
         postsRepository.deleteAll();
     }
 
     @Test
     void post를_저장조회한다() {
+        //given
         String title = "title";
         String content = "content";
-        Posts post = Posts.builder()
+        Posts savePost = Posts.builder()
                         .title(title)
                         .content(content)
                         .author("tester")
-                    .build();
+                        .build();
 
-        postsRepository.save(post);
-        postsRepository.findAll();
+        //when
+        postsRepository.save(savePost);
+        List<Posts> posts = postsRepository.findAll();
 
+        //then
+        Posts post = posts.get(0);
+        assertThat(post.getTitle()).isEqualTo(title);
+        assertThat(post.getContent()).isEqualTo(content);
     }
 }
