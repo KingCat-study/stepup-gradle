ecbdcd3a6259004def47af00fdf3727b2985faf9
raiden <jihunrock@gmail.com>
2023-01-20 23:11:54 +0900

    게시글 전체 목록 게시글 수정 기능 추가

--- src/main/java/com/jh/domain/posts/PostsRepository.java
+++ src/main/java/com/jh/domain/posts/PostsRepository.java
@@ -1,6 +1,13 @@
 package com.jh.domain.posts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p from Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+
 }
--- src/main/java/com/jh/web/IndexController.java
+++ src/main/java/com/jh/web/IndexController.java
@@ -1,18 +1,40 @@
 package com.jh.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.jh.service.PostsService;
+import com.jh.web.dto.PostsResponseDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String postSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(
+        @PathVariable Long id, 
+        Model model) {
+        
+            PostsResponseDto responseDto = postsService.findById(id);
+            model.addAttribute("post", responseDto);
+
+        return "posts-update";
     }
 }
