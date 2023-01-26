13732393fa6164605350d78e6c210e44567019e8
jihunrock <jihunrock@gmail.com>
2023-01-26 16:11:06 +0900

    -구글 Oauth 로그인 기능 구현 .로그인 시 username 을 model 에 저장

--- src/main/java/com/jh/web/IndexController.java
+++ src/main/java/com/jh/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jh.web;
 
+import com.jh.config.auth.dto.SessionUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +11,22 @@ import com.jh.web.dto.PostsResponseDto;
 
 import lombok.RequiredArgsConstructor;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
