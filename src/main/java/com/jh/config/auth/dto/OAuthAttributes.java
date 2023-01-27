f4ce09c98fd81bde6038c772f5df31b3060d7514
jihunrock <jihunrock@gmail.com>
2023-01-27 13:56:40 +0900

    -네이버 Oauth 로그인 기능 추가

--- src/main/java/com/jh/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jh/config/auth/dto/OAuthAttributes.java
@@ -4,6 +4,7 @@ import com.jh.domain.user.Role;
 import com.jh.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.util.StringUtils;
 
 import java.util.Map;
 
@@ -26,6 +27,10 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if(StringUtils.pathEquals("naver",registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -35,6 +40,18 @@ public class OAuthAttributes {
                 .email((String)attributes.get("email"))
                 .picture((String)attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String)response.get("name"))
+                .email((String)response.get("email"))
+                .picture((String)response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
