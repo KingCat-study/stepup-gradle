967fce6a4880809239501a1c381afa16637843ae
jihunrock <jihunrock@gmail.com>
2023-02-14 16:07:51 +0900

    배포 스크립트 Syntax 오류 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,5 +34,5 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar -Dspring.profiles.active=real -Dspring.config.location=classpath:/application.properties,
-/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties $JAR_NAME >
-$REPOSITORY/nohup.out 2>&1 &
+/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties
+$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
